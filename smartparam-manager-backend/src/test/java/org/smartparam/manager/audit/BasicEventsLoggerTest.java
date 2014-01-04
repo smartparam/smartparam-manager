1432ba529d0d9d94353f2608929803045debad75
Adam Dubiel <dubiel.adam@gmail.com>
2014-01-05 00:25:19 +0100

    basic events logger test

---
+++ smartparam-manager-backend/src/test/java/org/smartparam/manager/audit/BasicEventsLoggerTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2014 Adam Dubiel, Przemek Hertel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smartparam.manager.audit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.smartparam.editor.model.ParameterEntryKey;
+import org.smartparam.editor.model.simple.SimpleParameterEntryKey;
+import org.smartparam.engine.core.parameter.Parameter;
+import org.smartparam.engine.core.parameter.ParameterEntry;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.mockito.Mockito.*;
+import static org.smartparam.engine.core.parameter.ParameterEntryTestBuilder.parameterEntry;
+import static org.testng.Assert.fail;
+
+/**
+ *
+ * @author Adam Dubiel
+ */
+public class BasicEventsLoggerTest {
+
+    private EventLogRepository repository;
+
+    private EventLogEntryFactory factory;
+
+    private BasicEventsLogger logger;
+
+    @BeforeMethod
+    @SuppressWarnings("unchecked")
+    public void setUp() {
+        repository = mock(EventLogRepository.class);
+        factory = mock(EventLogEntryFactory.class);
+
+        when(repository.supports(any(Class.class))).thenReturn(true);
+        logger = new BasicEventsLogger(repository, factory);
+    }
+
+    @Test
+    public void shouldThrowUnsupportedEventTypeExceptionWhenLogRepositoryDoesNotSupportFactory() {
+        // given
+        doReturn(EventLogEntry.class).when(factory).produces();
+        when(repository.supports(EventLogEntry.class)).thenReturn(false);
+
+        // when
+        try {
+            new BasicEventsLogger(repository, factory);
+            fail();
+        } catch (EventLogEntryTypeNotSupported exception) {
+            // then passed
+            return;
+        }
+    }
+
+    @Test
+    public void shouldAskFactoryToProduceEventEntryAndSaveItWhenLoggingParameterCreation() {
+        // given
+
+        // when
+        logger.logParameterCreation(null, null);
+
+        // then
+        verify(factory).produceParameterCreationLog(any(EventDescription.class), any(Parameter.class));
+        verify(repository).save(any(EventLogEntry.class));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void shouldSaveAsManyEventsAsEntriesCreated() {
+        // given
+        List<ParameterEntryKey> keys = new ArrayList<ParameterEntryKey>(Arrays.asList(new SimpleParameterEntryKey("1"), new SimpleParameterEntryKey("2")));
+        List<ParameterEntry> entries = Arrays.asList(parameterEntry().build(), parameterEntry().build());
+
+        // when
+        logger.logEntryCreation(null, keys, entries);
+
+        // then
+        verify(factory, times(2)).produceEntryCreationLog(any(EventDescription.class), any(ParameterEntryKey.class), any(ParameterEntry.class));
+        verify(repository).save(anyList());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void shouldSaveAsManyEventsAsEntriesDeleted() {
+        // given
+        List<ParameterEntryKey> keys = new ArrayList<ParameterEntryKey>(Arrays.asList(new SimpleParameterEntryKey("1"), new SimpleParameterEntryKey("2")));
+        List<ParameterEntry> entries = Arrays.asList(parameterEntry().build(), parameterEntry().build());
+
+        // when
+        logger.logEntryDeletion(null, keys, entries);
+
+        // then
+        verify(factory, times(2)).produceEntryDeletionLog(any(EventDescription.class), any(ParameterEntryKey.class), any(ParameterEntry.class));
+        verify(repository).save(anyList());
+    }
+}
